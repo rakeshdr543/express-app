1f638ca4fc48fdab783a7bc116c17755ce523cf8
Rakesh D R <rakeshdr543@gmail.com>
2021-10-20 07:46:10 +0530

    Added db connect

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,11 +1,25 @@
 const express=require('express')
 
 const tasks=require('./routes/tasks.js')
+const connectDB=require('./db/connect.js')
+require('dotenv').config()
 
 const app=express()
 
+app.use(express.json())
+
 app.use('/api/v1/tasks',tasks)
 
-app.listen(5000,
-    console.log('running on 5000')
-)
+const start=async()=>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(5000,
+            console.log('running on 5000')
+        )
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
