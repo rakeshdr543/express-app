52642536ea6325fbc5ea6bcef3f8765ba27e2416
Rakesh D R <rakeshdr543@gmail.com>
2021-11-03 08:16:28 +0530

    Added security headers

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -2,6 +2,12 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 
+// extra security packages
+const helmet=require('helmet')
+const cors=require('cors')
+const xss=require('xss-clean')
+const rateLimiter=require('express-rate-limit')
+
 const authRouter=require('./routes/auth')
 const jobsRouter = require('./routes/jobs');
 
@@ -14,7 +20,17 @@ const authenticateUser = require('./middleware/authentication');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy',1)
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+)
 app.use(express.json());
+app.use(helmet())
+app.use(xss())
+app.use(cors())
 
 // routes
 app.use('/api/v1/auth', authRouter);
