4639e1f8de3d749b4de1b02c6b22e9ceec24d8be
Rakesh D R <rakeshdr543@gmail.com>
2021-11-06 11:15:34 +0530

    Added cloud storage

--- 07-file-upload/starter/app.js
+++ 07-file-upload/starter/app.js
@@ -6,6 +6,15 @@ const app = express();
 
 const fileUpload = require('express-fileupload');
 
+// USE V2
+const cloudinary=require('cloudinary').v2
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
 // database
 const connectDB = require('./db/connect');
 
@@ -19,7 +28,7 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 app.use(express.static('./public'))
 
 app.use(express.json());
-app.use(fileUpload())
+app.use(fileUpload({useTempFiles:true}))
 
 app.use('/api/v1/products',productRouter)
 
--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -1,9 +1,12 @@
 const path=require('path')
+const fs=require('fs')
+
 const {StatusCodes} = require('http-status-codes')
+const cloudinary=require('cloudinary').v2
 
 const CustomError = require('../errors')
 
-const uploadProductImage = async (req, res) => {
+const uploadProductImageLocal = async (req, res) => {
     if(!req.files){
         throw new CustomError.BadRequestError('No File Uploaded')
     }
@@ -24,6 +27,21 @@ const uploadProductImage = async (req, res) => {
     await productImage.mv(imagePath)
 
     return res.status(StatusCodes.OK).json({image:{src:`/uploads/${productImage.name}`}})
+}
+
+const uploadProductImage = async (req, res) => {
+
+    const result = await cloudinary.uploader.upload(
+        req.files.image.tempFilePath,
+        {
+            use_filename:true,
+            folder:'file-upload'
+        }
+    )
+
+    fs.unlinkSync(req.files.image.tempFilePath)
+
+    return res.status(StatusCodes.OK).json({image:{src:result.secure_url}})
 }
 
 module.exports={uploadProductImage}
